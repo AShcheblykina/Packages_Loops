a6c60858130414748a3e7507b92697b48a17a9fd
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2022-12-07 21:57:52 +0300

    changed src

--- src/main/java/ru/netology/sqr/Packages_Loops/SQRService/calcSqr/SQRService.java
+++ src/main/java/ru/netology/sqr/Packages_Loops/SQRService/calcSqr/SQRService.java
@@ -3,10 +3,12 @@ package ru.netology.sqr.Packages_Loops.SQRService.calcSqr;
 public class SQRService {
 
     public int calcSqr(int x) {
-        for (int i = 10; i <= x; i++) {
+        int bottomline = 400;
+        int upperbound = 500;
+        for (int i = 10; i <= 99; i++) {
             if (i * i >= x)
-                if (x >= 400)
-                    if (x <= 500) {
+                if (bottomline >= 400)
+                    if (upperbound <= 500) {
                         return i;
                     }
         }
